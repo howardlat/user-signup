ab7a0fd7838ee1edeff146595716ffc56be64fb1
Latasha Howard <l_howard@outlook.com>
2018-10-03 18:17:10 -0500

    error

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/username", methods=['Post'])
-def username():
+@app.route("/error", methods=['POST'])
+def error():
     username = request.form['username']
-  
-    if len(username) < 3 or len(username)> 20:
-        return redirect ("/?error=")
-           
+    
+    if not username.isalpha():
+        error = "That's not a valid username"
+    elif len(username) <3 or len(username) > 20:
+        return redirect("/?error=" + error)
         
 
-
+       
 @app.route("/")
 def index():
-    return render_template('base.html')
+    encoded_error = request.args.get("error")
+    return render_template('base.html', error=encoded_error)
 
 app.run()
