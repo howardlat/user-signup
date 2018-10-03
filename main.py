4a76aab032ab3a7943b7d95c513e251e381583cb
Latasha Howard <l_howard@outlook.com>
2018-10-03 15:24:24 -0500

    validate

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route("/username", methods=['Post'])
+def username():
+    username = request.form['username']
+  
+    if len(username) < 3 or len(username)> 20:
+        return redirect ("/?error=")
+           
+        
+
+
+@app.route("/")
+def index():
+    return render_template('base.html')
+
+app.run()
