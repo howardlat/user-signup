b76c8fc248e396e62294432ac1a320c739206d04
Latasha Howard <l_howard@outlook.com>
2018-10-03 22:06:20 -0500

    changes

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('validate', methods=['POST'])
+@app.route('/validate', methods=['POST'])
 def validate():
     username = request.form['username']    
     if not username.isalpha() or len(username) <3 or len(username) > 20:
         error = "That's not a valid username"
-        return redirect("/?error=" + error)  
-
-        
+        return render_template('base.html')  + error     
    
        
 @app.route("/")
 def index():
-   return render_template('base.html')
+    encoded_error = request.args.get("error")
+    return render_template('base.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
