be33a80e6a7992336ab8207cc1e6be2465a81822
Latasha Howard <l_howard@outlook.com>
2018-10-03 20:22:25 -0500

    changes

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/validate', methods=['POST', 'GET'])
+@app.route('/validate', method=['POST', 'GET'])
 def validate():
     username = request.form['username']    
     if not username.isalpha() or len(username) <3 or len(username) > 20:
         error = "That's not a valid username"
-        return error   
+        return redirect("/?error=" + error)  
 
         
    
