990e17e417db9f8fa5e59858a2afd81176bc157e
Latasha Howard <l_howard@outlook.com>
2018-10-05 13:55:18 -0500

    changes

--- main.py
+++ main.py
@@ -10,42 +10,28 @@ def validate():
     user_error = ""
     if " " in username or len(username) == 0 or len(username) <3 or len(username) > 20:
         user_error = "That's not a valid username"
+        redirect("/?error=", user_error)
     
     password = request.form['password']
     password_error = ""
     if " " in password or len(password) == 0 or len(password) <3 or len(password) > 20:
         password_error = "That's not a valid password"
+        redirect("/?error=", password_error)
         
     verify = request.form['verify']
     verify_error = ""
     if " " in verify or len(verify) == 0 or verify != password: 
         verify_error = "Passwords don't match"
+        redirect("/?error=", verify_error)
         
     email = request.form['email']
     email_error = ""
     if "@" and " " and "." not in email:
         email_error = "Invalid email address"
+        redirect("/?error=", email_error)
 
-        return render_template('base.html',
-        user_error=user_error,
-        password_error=password_error,
-        verify_error=verify_error,
-        email_error=email_error,
-        username=username)   
-
-@app.route('/welcome', methods=['POST'])
-def welcome():
-    user_error = ''
-    password_error = ''
-    verify_error = ''
-    email_error = ''
-    username = ''
     if not user_error and not password_error and not verify_error and not email_error:
         return render_template('welcome.html',
-        user_error=user_error,
-        password_error=password_error,
-        verify_error=verify_error,
-        email_error=email_error,
         username=username)
          
 @app.route("/")
